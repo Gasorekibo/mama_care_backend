3397922d92b5878aa00ef1713adda3b16215d41e
Gasore Mugwaneza <113452888+Gasorekibo@users.noreply.github.com>
2025-02-28 21:24:35 +0200

    Update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -8,7 +8,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-       url: process.env.DATABASE_URL
+       url: process.env.DATABASE_URL,
       entities: ['dist/**/**/*.entity.{ts,js}'],
       synchronize: true,
     };
